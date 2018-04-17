f02c23c62055573cf4c85053c670aed5c512fff1
Michael Gerov <mhgerov@gmail.com>
2018-04-17 16:07:26 -0400

    connected to db

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
-const PORT = process.env.PORT || 3000;
-
+//import modules 
+require('dotenv').config();
 var inquirer = require('inquirer');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host:		process.env.DB_HOST,
+	user:		process.env.DB_USER,
+	password:	process.env.DB_PASSWD,
+	database:	process.env.DB
+});
+connection.connect();
 
+//Commonly used queries
+const fullInventory = 'SELECT product_name, department_name, price FROM bamazon_db.products';
+
+//Welcome message
 console.log('Welcome to Bamazon!');
 
+//Show inventory
+connection.query(fullInventory,function (err,res) {
+	if (err) throw err;
+	console.log(res);
+});
+
+//End of main execution
+connection.end();
+/*
 var bidPost = [
 	{
 	type: 'list',
@@ -19,3 +40,4 @@ var bidPost = [
 inquirer.prompt(bidPost).then(function(answers) {
 	console.log(answers.bidOrPost);
 });
+*/
