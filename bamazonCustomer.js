b2506ac869bdcb3757ed6adbbe0c9209873056ae
Michael Gerov <mhgerov@gmail.com>
2018-04-17 20:46:31 -0400

    bamazonCustomer complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,17 +12,16 @@ connection.connect();
 
 //Commonly used queries
 const fullInventory = 'SELECT item_id, product_name, price FROM bamazon_db.products';
+const id_list = 'SELECT item_id FROM bamazon_db.products';
 
 //Welcome message
 console.log('Welcome to Bamazon!');
 
 //main execution
-listInventory(orderPage);
+listInventory();
 
-//End of main execution
-connection.end();
 
-function listInventory(cb) {
+function listInventory() {
 	connection.query(fullInventory,function (err,res,fields) {
 		if (err) throw err;
 		for (i in res) {
@@ -31,10 +30,54 @@ function listInventory(cb) {
 						'Price: $'+res[i].price);
 			console.log('-----------------------');
 		}
-		cb();
+		inquireOrder();
 	});
 }
 
-//Should return a valid item# and quantiy
-function orderPage() {
+function inquireOrder() {
+	connection.query(id_list, function (err,res) {
+		//Pull list of item IDs
+		if (err) throw err;
+		var list = [];
+		for (i in res) {
+			list.push(String(res[i].item_id));
+		}
+		//Ask for item and quantity
+		inquirer.prompt([
+			{
+				type: 'list',
+				name: 'item',
+				message: 'Which item would you like to order?',
+				choices: list,
+			},{
+				type: 'input',
+				name: 'quantity',
+				message: 'How much of the item would you like to order?',
+				default: 1
+			}
+		]).then(answers => {
+			validateOrder(answers);
+		}).catch( err => console.log(err));
+	});
+}
+
+function validateOrder(answers) {
+	connection.query('SELECT product_name, stock_quantity ,price FROM bamazon_db.products WHERE item_id=?', [answers.item],function (err,res) {
+		var qty_req = answers.quantity	
+		var qty_avail = res[0].stock_quantity;
+		var unit_price = res[0].price;
+		var product_name = res[0].product_name;
+		if (qty_avail>=qty_req) {
+			console.log('Executing order...');
+			connection.query('UPDATE bamazon_db.products SET stock_quantity=? WHERE item_id=?', [qty_avail-qty_req,answers.item], function (err) {
+				if (err) throw err;
+				console.log('Your total: $'+unit_price*qty_req);
+				connection.end();
+			});
+			
+		} else {
+			console.log('Insufficient quantity available!');
+			connection.end();
+		}
+	});
 }
